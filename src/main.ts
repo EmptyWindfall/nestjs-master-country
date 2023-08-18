1b621170c351bcad99c6ef3becedaca3e636f1c2
Mean <phatthanasak.w@globish.co.th>
2023-08-18 20:06:51 +0700

    adding swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Protoss Technology')
+    .setDescription('Testing Master Country Loing APIs with MongoDB')
+    .setVersion('0.1Alpha')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  await app.listen(3000, () => {
+    console.log('Swagger Local: http://localhost:3000/api')
+  });
 }
 bootstrap();
